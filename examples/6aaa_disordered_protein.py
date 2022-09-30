0baca291cb0c0cc53a499f2d29800f1527e79f59
Radost Waszkiewicz <radost.waszkiewicz@gmail.com>
2022-09-30 12:33:33 +0200

    6aaa plot formatting

--- examples/6aaa_disordered_protein.py
+++ examples/6aaa_disordered_protein.py
@@ -4,6 +4,21 @@ import saxs_single_bead.scattering_curve
 import sarw_spheres
 import matplotlib.pyplot as plt
 
+plt.rcParams.update({
+    "legend.frameon": False,
+    "text.usetex": True,
+    "mathtext.fontset": "cm",
+    "font.family": "serif",
+    "font.serif": ["Palatino"],
+    "font.size" : 15,
+    "figure.subplot.left":   0.125,
+    "figure.subplot.right":  0.95,
+    "figure.subplot.bottom": 0.15,
+    "figure.subplot.top":    0.95,
+    "figure.subplot.wspace": 0.2,
+    "figure.subplot.hspace": 0.2,
+})
+
 # Sequence of the 6AAA protein
 sequence = "VRTKADSVPGTYRKVVAARAPRKVLGSSTSATNSTSVSSRKAENKYAGGNPVCVRPTPKWQKGIGEFFRLSPKDSEKENQIPEEAGSSGLGKAKRKACPLQPDHTNDEKE"
 ensemble_size = 100
